9910b08fd09d1c206613c3339f7a83e786a4e0bf
mnichangxin <mnichangxin@vip.qq.com>
2016-09-23 19:17:55 +0800

    first post

---
+++ js/data.js
@@ -0,0 +1,57 @@
+//模拟JSON对象
+var data = [
+	{
+		"title": 'Demo1',
+		"content": 'This is demo1 ',
+		"demo": '',
+		"origin": ''
+	},
+	{
+		"title": 'Demo1',
+		"content": 'This is demo1 ',
+		"demo": '',
+		"origin": ''
+	},
+	{
+		"title": 'Demo1',
+		"content": 'This is demo1 ',
+		"demo": '',
+		"origin": ''
+	},
+	{
+		"title": 'Demo1',
+		"content": 'This is demo1 ',
+		"demo": '',
+		"origin": ''
+	},
+	{
+		"title": 'Demo1',
+		"content": 'This is demo1 ',
+		"demo": '',
+		"origin": ''
+	},
+	{
+		"title": 'Demo1',
+		"content": 'This is demo1 ',
+		"demo": '',
+		"origin": ''
+	},
+	{
+		"title": 'Demo1',
+		"content": 'This is demo1 ',
+		"demo": '',
+		"origin": ''
+	},
+	{
+		"title": 'Demo1',
+		"content": 'This is demo1 ',
+		"demo": '',
+		"origin": ''
+	},
+	{
+		"title": 'Demo1',
+		"content": 'This is demo1 ',
+		"demo": '',
+		"origin": ''
+	}
+];
