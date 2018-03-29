72b3b914ac9df44412cd2a4e28ff9f4c47675733
mnichangxin <mnichangxin@163.com>
2018-03-29 18:00:37 +0800

    commit

--- scrollfix/index.js
+++ scrollfix/index.js
@@ -1,27 +1,53 @@
 /* 修复滚动出界 */
-var ScrollFix = function(elem) {
-	var startY, startTopScroll
+// var ScrollFix = function(elem) {
+// 	var startY, startTopScroll
 	
-    elem = elem || document.querySelector(elem)
+//     elem = elem || document.querySelector(elem)
     
-	if (!elem) {
-        return        
-    }
+// 	if (!elem) {
+//         return        
+//     }
 
-    document.addEventListener('touchmove', function(event) {
-        event.preventDefault()
-    })
+//     document.addEventListener('touchmove', function(event) {
+//         event.preventDefault()
+//     })
 
-	// elem.addEventListener('touchstart', function(event) {
-	// 	startY = event.touches[0].pageY
-	// 	startTopScroll = elem.scrollTop
+// 	// elem.addEventListener('touchstart', function(event) {
+// 	// 	startY = event.touches[0].pageY
+// 	// 	startTopScroll = elem.scrollTop
 		
-	// 	if (startTopScroll <= 0) {
-	// 		elem.scrollTop = 1            
-    //     }
+// 	// 	if (startTopScroll <= 0) {
+// 	// 		elem.scrollTop = 1            
+//     //     }
 
-	// 	if (startTopScroll + elem.offsetHeight >= elem.scrollHeight) {
-	// 		elem.scrollTop = elem.scrollHeight - elem.offsetHeight - 1            
-    //     }
-	// }, false)
+// 	// 	if (startTopScroll + elem.offsetHeight >= elem.scrollHeight) {
+// 	// 		elem.scrollTop = elem.scrollHeight - elem.offsetHeight - 1            
+//     //     }
+// 	// }, false)
+// }
+
+var Overscroll = function(el) {
+    el.addEventListener('touchstart', function() {
+        var top = el.scrollTop
+            , totalScroll = el.scrollHeight
+            , currentScroll = top + el.offsetHeight;
+
+        if (top === 0) {
+            el.scrollTop = 1;
+        } else if (currentScroll === totalScroll) {
+            el.scrollTop = top - 1;
+        }
+    });
+    el.addEventListener('touchmove', function(evt) {
+        if (el.offsetHeight < el.scrollHeight)
+            evt._isScroller = true;
+    });
 }
+
+// overscroll(document.querySelector('.scroll'));
+
+document.body.addEventListener('touchmove', function(evt) {
+    if (!evt._isScroller) {
+        evt.preventDefault();
+    }
+});
