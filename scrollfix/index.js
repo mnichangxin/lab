ca72ee2fa71673c34a14c9a774aa0064722a02af
mnichangxin <mnichangxin@163.com>
2018-03-29 15:45:54 +0800

    commit

---
+++ scrollfix/index.js
@@ -0,0 +1,23 @@
+/* 修复滚动出界 */
+var ScrollFix = function(elem) {
+	var startY, startTopScroll
+	
+    elem = elem || document.querySelector(elem)
+    
+	if (!elem) {
+        return        
+    }
+
+	elem.addEventListener('touchstart', function(event) {
+		startY = event.touches[0].pageY
+		startTopScroll = elem.scrollTop
+		
+		if (startTopScroll <= 0) {
+			elem.scrollTop = 1            
+        }
+
+		if (startTopScroll + elem.offsetHeight >= elem.scrollHeight) {
+			elem.scrollTop = elem.scrollHeight - elem.offsetHeight - 1            
+        }
+	}, false)
+}
