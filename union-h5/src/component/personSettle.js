a060748ced79d4ec433f42e5bf0dd3be3855af04
mnichangxin <mnichangxin@163.com>
2018-04-16 14:37:34 +0800

    add select

--- union-h5/src/component/personSettle.js
+++ union-h5/src/component/personSettle.js
@@ -1,6 +1,106 @@
+/* 结算管控 */
 import React from 'react'
 import {Link} from 'react-router-dom'
 import 'whatwg-fetch'
+
+class Select extends React.Component {
+    constructor(props) {
+        super(props)
+        
+        this.state = {
+            index: 0
+        }
+
+        this.handleClick = this.handleClick.bind(this)
+    }
+
+    handleClick(index, e) {
+        this.setState({
+            index: index
+        })
+
+        setTimeout(() => {
+            this.props.changeStatus()
+        }, 50)
+    } 
+
+    render() {
+        return (
+            <div className="select">
+                {
+                    this.props.items.map(
+                        (item, index) => {
+                            return (
+                                <p className={this.state.index == index ? 'c-mark' : ''} key={index} onClick={(e) => this.handleClick(index, e)}>{item}<i className="choosed-right"></i></p>
+                            )
+                        }
+                    )
+                }
+            </div>
+        )
+    }
+}
+
+class MoneyBox extends React.Component {
+    constructor(props) {
+        super(props)
+    }
+
+    render() {
+        return (
+            <div>
+                <div className="m-content">
+                    <ul className="mes-list">
+                        <li>
+                            <div className="c-name">结算周期：</div>
+                            <div className="c-mes">2018/01/01 - 2018/03/01</div>
+                        </li>
+                        <li>
+                            <div className="list-two">
+                                <div className="list-left">
+                                    <div className="c-name">申请ID：</div>041545478478
+                                </div>
+                                <div className="list-right">
+                                    <div className="c-name">申请日期：</div>2018/03/01
+                                </div>
+                            </div>
+                        </li>
+                        <li>
+                            <div className="list-two">
+                                <div className="list-left">
+                                    <div className="c-name">申请状态：</div>
+                                    <span className="c-circle green"></span> 有效
+                                </div>
+                                <div className="list-right">
+                                    <div className="c-name">结算状态：</div>
+                                    <span className="c-circle gray"></span> 未結算
+                                </div>
+                            </div>
+                        </li>
+                        <li>
+                            <div className="c-name">电子钱包账户：</div>
+                            <div className="c-mes">45456484541</div>
+                        </li>
+
+                        <li>
+                            <div className="c-name">收款人身份证：</div>
+                            <div className="c-mes">041545478478</div>
+                        </li>
+                    </ul>
+                </div>
+                <div className="money-box">
+                    <div className="money">
+                        返佣金额 <span className="c-mark">￥1545.57</span>
+                    </div>
+                    <div className="check-btn">
+                        <a href="" className="job-btn cancel">撤消申请</a>
+                        <a href="" className="job-btn disabled">查看业绩</a>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
 
 class PersonSettle extends React.Component {
     constructor(props) {
@@ -12,6 +112,14 @@ class PersonSettle extends React.Component {
         }
 
         this.handleSelect = this.handleSelect.bind(this)
+        this.changeStatus = this.changeStatus.bind(this)
+    }
+
+    changeStatus(status) {
+        this.setState({
+            invite_status: false,
+            settle_status: false
+        })
     }
 
     handleSelect(id, e) {
@@ -27,14 +135,18 @@ class PersonSettle extends React.Component {
                     invite_status: false,
                     settle_status: true
                 })
-                this
                 break
+            case 'cover_status':
+                this.setState({
+                    invite_status: false,
+                    settle_status: false
+                })
         }
     }
 
     render() {
         return (
-            <div>
+            <div style={{height: '100%'}}>
                 <div className="m-box m-box-top m-box-top-green">
                     <div className="m-box-items m-box-items-full">
                         <section className="m-topSite m-borderB">
@@ -44,30 +156,23 @@ class PersonSettle extends React.Component {
                     </div>
                 </div>
                 <div className="account-box">
+                    <div className={this.state.invite_status || this.state.settle_status ? 'cover' : ''} onClick={(e) => this.handleSelect('cover_status', e)}></div>
                     <div className="m-nav">
                         <ul className="balance-box">
                             <li className={this.state.invite_status ? 'clicked' : ''} onClick={(e) => this.handleSelect('invite_status', e)}>
                                 申请状态 <i className="select-icon"></i>
-                                <div className="select">
-                                    <p className="c-mark">全部</p>
-                                    <p>有效</p>
-                                    <p>已撤销</p>
-                                    <p>已过期</p>
-                                </div>
+                                <Select items={['全部', '有效', '已撤销', '已过期']} changeStatus={this.changeStatus} />
                                 <div className="float-fill"></div>
                             </li>
                             <li className={this.state.settle_status ? 'clicked' : ''} onClick={(e) => this.handleSelect('settle_status', e)}>
                                 结算状态 <i className="select-icon"></i>
-                                <div className="select">
-                                    <p className="c-mark">全部</p>
-                                    <p>已结算</p>
-                                    <p>未结算</p>
-                                </div>
+                                <Select items={['全部', '已结算', '未结算']} changeStatus={this.changeStatus} />
                                 <div className="float-fill"></div>
                             </li>
                         </ul>
                     </div>
-                    <div className="m-content">
+                    <MoneyBox />
+                    {/*<div className="m-content">
                         <ul className="mes-list">
                             <li>
                                 <div className="c-name">结算周期：</div>
@@ -99,7 +204,7 @@ class PersonSettle extends React.Component {
                                 <div className="c-name">电子钱包账户：</div>
                                 <div className="c-mes">45456484541</div>
                             </li>
-
+ 
                             <li>
                                 <div className="c-name">收款人身份证：</div>
                                 <div className="c-mes">041545478478</div>
@@ -230,7 +335,7 @@ class PersonSettle extends React.Component {
                             <span className="c-mark c-normal">取消</span>
                             <span className="c-mark">提交</span>
                         </div>
-                    </div>
+                    </div> */}
                     <div className="settle-accounts hide">
                         <div className="c-title">申请结算</div>
                         <p className="c-word">是否确认撤销此申请，<br />撤销后需要重新提交结算申请</p>
