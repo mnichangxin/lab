6884abca190a1f7e367a0d26e786670c98b43feb
mnichangxin <mnichangxin@163.com>
2018-08-01 14:18:51 +0800

    commit

--- vue-learning/index.js
+++ vue-learning/index.js
@@ -1,11 +1,28 @@
+Vue.component('button-container', {
+    data: function() {
+        return {
+            count: 0
+        }
+    },
+    template: '<button v-on:click="count++">You click me {{ count }} count.</button>'
+})
+
+Vue.component('blog-post', {
+    props: ['title'],
+    template: '<h3>{{ title }}</h3>'
+})
+
+Vue.component('alert', {
+    template: `
+        <p>This is a alert component.</p>
+    `
+})
+
 var app = new Vue({
     el: '#app',
     data: {
         list: [],
         item: ''
-    },
-    computed: {
-        
     },
     methods: {
         add: function() {
