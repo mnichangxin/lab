b30291ce688b2aa940de1e492ff594712784082a
mnichangxin <mnichangxin@163.com>
2019-02-11 12:25:32 +0800

    update union-h5

--- union-h5/webpack.config.js
+++ union-h5/webpack.config.js
@@ -5,7 +5,7 @@ const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
 
 module.exports = {
     mode: 'development',
-    entry: ['./app.js'],
+    entry: path.resolve(__dirname, 'app.js'),
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'build'),
